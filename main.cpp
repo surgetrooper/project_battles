493e683e09552fea0acc3ce316ef3d042f80caad
surgetrooper <87729460+surgetrooper@users.noreply.github.com>
2021-12-18 21:49:46 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,229 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+//code by HARSH BHARWANI-I005-MBA.TECH IT
+#include <iostream>
+using namespace std;
+class pj
+{
+    public:
+    void password()
+    {
+    int ch;
+    string yn,dat,faxx,opt;
+    int a;
+    cout<<"Enter the password: "<<endl; //52
+    cin>>a;
+    if (a==52)
+    {
+        cout<<"correct."<<endl;
+        for (int i=0;i<=1000;i++)
+        {
+            cout<<"Initialising. . ."<<endl;
+        }
+        for (int i=0;i<=100000;i++)
+        {
+            cout<<"52";
+        }
+        cout<<endl;
+        cout<<"Welcome to Dimension 52, a place with.... well, 52 related stuff: 2 endings will lead you to the gods."<<endl;
+        cout<<"So, what do you wanna do?"<<endl;
+        //enter number
+        cout<<"1. a fact"<<endl;
+        cout<<"2. a random execution"<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1: 
+            {
+                cout<<"so, you want a fact about 52?"<<endl;
+                cout<<"argh, its a bummer i dont know any. its only a number after all."<<endl;
+                cout<<"do you? (enter only y or n. orrrr its upto you. :)"<<endl;
+                cin>>yn;
+                if(yn=="y")
+                {
+                    string ak;
+                    cout<<"why the wait? tell me."<<endl;
+                    cin>>faxx;
+                    cout<<"that doesnt look like much of a fact."<<endl;
+                    cout<<"well whatever, props to you for trying."<<endl;
+                    cout<<"\nthe 52 gods of Dimension 52 reward you for your efforts. \n\n";
+                    cout<<"\npress any number"<<endl;
+                    cin>>ak;
+                    cout<<endl;
+                    cout<<"*a portal opens*"<<endl;
+                    blessed();
+                    cout<<"\nEnding 2/6 reached."<<endl;
+                    break;
+                }
+                else if (yn=="n")
+                {
+                    cout<<"thats not very good."<<endl;
+                    cout<<"i dont really like you, you know."<<endl;
+                    cout<<"Ending 3/6 reached."<<endl;
+                    break;
+                }
+                else
+                {
+                    cout<<"got a rule breaker on our hands huh??"<<endl;
+                    cout<<"i like you."<<endl;
+                    cout<<"wanna go on a date sometime? (y/n forreal this time)"<<endl;
+                    cin>>dat;
+                    if (dat=="y")
+                    {
+                        cout<<"im only a program, dummy. ask my creator."<<endl;
+                        cout<<"BONUS ENDING REACHED. feel free to contact me, how does 5/2/22 sound? ;)"<<endl;
+                    }
+                    else
+                    {
+                        cout<<"that is... disappointing. :( "<<endl;
+                        cout<<"oh well."<<endl;
+                        cout<<"Ending 4/6 reached."<<endl;
+                    }
+                }
+            break;
+            }
+            case 2:
+            {
+                cout<<"a random execution it is."<<endl;
+                cout<<"Ending 5/6 reached . .?"<<endl;
+                cout<<endl;
+                for(int i=0; i<=500000000;i++)
+                {
+                    cout<<"";
+                }
+                cout<<"SIKE! not done yet."<<endl;
+                cout<<"A crowd from the 1880s arrives as you execute a guy named Phil for stealing bread."<<endl;
+                cout<<"*you chop his head off with your axe*"<<endl;
+                cout<<"You're evil. I dont like you."<<endl;
+                cout<<"execute more people? (y/n)"<<endl;
+                cin>>opt;
+                if (opt=="y")
+                {
+                    cout<<"a pirate is next. you get ready for him. \n suddenly a cannon is shot at you."<<endl;
+                    cout<<"welp. youre dead."<<endl;
+                    cout<<"gotta love jack sparrow."<<endl;
+                    cout<<"Ending 5/6 reached."<<endl;
+                }
+                else if (opt=="n")
+                {
+                    string ak;
+                    cout<<"It is the 52nd person to be executed today."<<endl;
+                    cout<<"You're very kind. but you forget you're an executioner."<<endl;
+                    cout<<"The king gives an order to execute you. But suddenly a beam of light shines upon you and you see a portal."<<endl;
+                    cout<<"\npress any number"<<endl;
+                    cin>>ak;
+                    cout<<endl;
+                    blessed();
+                    cout<<"\nEnding 6/6 reached."<<endl;
+                }
+                break;
+            }
+            default: 
+            {
+                    cout<<"got a rule breaker on out hands huh??"<<endl;
+                    cout<<"i like you."<<endl;
+                    cout<<"wanna go on a date sometime? (y/n forreal this time)"<<endl;
+                    cin>>dat;
+                    if (dat=="y")
+                    {
+                        cout<<"im only a program, dummy. ask my creator."<<endl;
+                        cout<<"BONUS ENDING REACHED. (feel free to contact me)"<<endl;
+                    }
+                    else
+                    {
+                        cout<<"that is... disappointing. :( "<<endl;
+                        cout<<"oh well."<<endl;
+                        cout<<"Ending 4/6 reached."<<endl;
+                    }
+            }
+        }
+    }
+    else
+    {
+        cout<<". . . . you're better than this. read the comments."<<endl;
+        cout<<"Ending 1/6 reached."<<endl;
+    }
+}
+void blessed ()
+{
+    int k,t=6,ctr=0,a=52;
+    string ak;
+                    for (int i=1;i<=5;i++)
+                    {
+                        t--;
+                        for (int j=1;j<=t;j++)
+                        {
+                            cout<<a;
+                        }
+                        ctr++;
+                        for(k=1;k<ctr;k++)
+                        {
+                            cout<<"    ";
+                        }
+                        for (int j=1;j<=t;j++)
+                        {
+                            cout<<a;
+                        }
+                        for (int tim=0;tim<30000000;tim++)
+                        {
+                            cout<<"";
+                        }
+                        cout<<endl;
+                    }
+                    t=0,ctr=6;
+                    for (int i=1;i<=5;i++)
+                    {
+                        
+                        t++;
+                        for (int j=1;j<=t;j++)
+                        {
+                            cout<<a;
+                        }
+                        ctr--;
+                        for(k=1;k<ctr;k++)
+                        {
+                            cout<<"    ";
+                        }
+                        for (int j=1;j<=t;j++)
+                        {
+                            cout<<a;
+                        }
+                        for (int tim=0;tim<30000000;tim++)
+                        {
+                            cout<<"";
+                        }
+                        cout<<endl;
+                    }
+                     cout<<"You have been blessed by the 52 gods of Dimension 52. \n and for that, you get a movie ticket for spiderman."<<endl;
+                    cout<<"Be grateful, you know how expensive it is?!"<<endl;
+                    cout<<"enter any number to get the ticket."<<endl;
+                    cin>>ak;
+                    cout<<endl;
+                    string sp;
+                    for(int i=0;i<13;i++)
+                    cout<<"52";
+                    cout<<endl;
+                    cout<<" 5252                5252"<<endl;
+                    cout<<"5252      ticket      5252"<<endl;
+                    cout<<" 5252                5252"<<endl;
+                    for(int i=0;i<13;i++)
+                    cout<<"52";
+                    cout<<"\nenjoy. want a spoiler? (y/n)"<<endl;
+                    cin>>sp;
+                    if(sp=="y")
+                    cout<<"aunt may dies."<<endl;
+                    else if(sp=="n")
+                    cout<<"no problem. im nice."<<endl;
+}
+};
+int main()
+{
+    pj p;
+    p.password();
+    return 0;
+}
